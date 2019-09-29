be54a3f5b1ba7bbb68f0c68a74dbc4dd66f8be25
VizerAM <anthonyvizer@gmail.com>
2019-09-29 07:19:02 +0300

    tilemap 	- SanyLandForest 	- SunnyLandWwoods 	- MagicCliffsArtwork

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+
+    private float legth, starpos;
+    public GameObject Cam;
+    public float paralaxEffect;
+
+    private void Awake()
+    {
+        starpos = transform.position.x;
+        legth = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = Cam.transform.position.x * (1 - paralaxEffect);
+
+        float dist = Cam.transform.position.x * paralaxEffect;
+
+        transform.position = new Vector3(starpos + dist, transform.position.y);
+
+        if(temp > starpos + legth)
+        {
+            starpos += legth;
+        }
+        else if(temp < starpos - legth)
+        {
+            starpos -= legth;
+        }
+    }
+}
