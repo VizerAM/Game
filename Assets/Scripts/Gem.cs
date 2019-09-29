259de6ecdf7094d8f43bb09b90aaeaba2a467811
VizerAM <anthonyvizer@gmail.com>
2019-09-29 11:02:16 +0300

    items 	- Acorn 	- Carrot 	- Gem 	- star 	- Cherry

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    public GameObject PickUpEfect;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(PickUpEfect).transform.position = transform.position;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Сherry.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Сherry : MonoBehaviour
+{
+    public GameObject PickUpEfect;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(PickUpEfect).transform.position = transform.position;
+        Destroy(gameObject);
+    }
+
+}
